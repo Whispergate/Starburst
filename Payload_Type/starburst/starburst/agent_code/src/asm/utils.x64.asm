b0365af3ac57a52fe7688f580fb8666636a5de87
Lav <l.m@securescape.cc>
2026-09-22 13:21:28 +0200

    Fixed __chkstk_ms stackprobe and __divdi3 division

--- Payload_Type/starburst/starburst/agent_code/src/asm/utils.x64.asm
+++ Payload_Type/starburst/starburst/agent_code/src/asm/utils.x64.asm
@@ -3,6 +3,27 @@
 DEFAULT REL
 
 GLOBAL RipData
+GLOBAL ___chkstk_ms
+GLOBAL __chkstk_ms
+
+[SECTION .text$B]
+    ___chkstk_ms:
+    __chkstk_ms:
+        push    rcx
+        push    rax
+        cmp     rax, 0x1000
+        lea     rcx, [rsp + 0x18]
+        jb      .cs_done
+    .cs_loop:
+        sub     rcx, 0x1000
+        test    dword [rcx], eax
+        sub     rax, 0x1000
+        cmp     rax, 0x1000
+        ja      .cs_loop
+    .cs_done:
+        pop     rax
+        pop     rcx
+        ret
 
 [SECTION .text$C]
     RipData:
--- Payload_Type/starburst/starburst/agent_code/src/intrinsics.cc
+++ Payload_Type/starburst/starburst/agent_code/src/intrinsics.cc
@@ -40,13 +40,61 @@ size_t_pic wcslen( const wchar_t* s ) {
     return len;
 }
 
+#ifndef _WIN64
+
+typedef unsigned long long u64_div;
+typedef long long          s64_div;
+
 __attribute__((section(".text$B")))
-void ___chkstk_ms( void ) {
+u64_div __udivdi3( u64_div num, u64_div den )
+{
+    if ( den == 0 || den > num )
+        return 0;
+
+    u64_div quot = 0;
+    u64_div rem  = 0;
+
+    for ( int i = 63; i >= 0; i-- )
+    {
+        rem = ( rem << 1 ) | ( ( num >> i ) & 1 );
+        if ( rem >= den )
+        {
+            rem  -= den;
+            quot |= ( 1ULL << i );
+        }
+    }
+    return quot;
 }
 
 __attribute__((section(".text$B")))
-void __chkstk_ms( void ) {
+s64_div __divdi3( s64_div a, s64_div b )
+{
+    int neg = 0;
+    u64_div ua, ub;
+
+    if ( a < 0 ) { ua = (u64_div)( -a ); neg = 1; }
+    else         { ua = (u64_div)a; }
+
+    if ( b < 0 ) { ub = (u64_div)( -b ); neg ^= 1; }
+    else         { ub = (u64_div)b; }
+
+    u64_div result = __udivdi3( ua, ub );
+    return neg ? -(s64_div)result : (s64_div)result;
 }
+
+__attribute__((section(".text$B")))
+u64_div __umoddi3( u64_div num, u64_div den )
+{
+    return num - __udivdi3( num, den ) * den;
+}
+
+__attribute__((section(".text$B")))
+s64_div __moddi3( s64_div a, s64_div b )
+{
+    return a - __divdi3( a, b ) * b;
+}
+
+#endif // !_WIN64
 
 #pragma clang diagnostic pop
 
